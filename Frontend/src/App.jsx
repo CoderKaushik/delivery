be65d0b44e35e2df85eba0f3cbda385f1b2f8d71
HiteshwarKaushik <thehiteshwarkaushik@gmail.com>
2024-03-27 21:51:35 +0530

    added frontend code

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import { Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
