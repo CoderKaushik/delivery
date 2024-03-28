7ecd5b300fe41500c79edb0eb8b63c8430ac05b8
HiteshwarKaushik <thehiteshwarkaushik@gmail.com>
2024-03-28 14:55:24 +0530

    complete frontend code added

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
             <img src={assets.logo} alt="" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam nemo aperiam alias hic mollitia? Eveniet adipisci odit harum vel explicabo!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam nemo aperiam alias hic mollitia? Eveniet adipisci odit harum vel explicabo! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem beatae voluptas mollitia voluptates ducimus qui, obcaecati eos a consequatur voluptatibus!</p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" /><img src={assets.twitter_icon} alt="" /><img src={assets.linkedin_icon} alt="" />
             </div>
